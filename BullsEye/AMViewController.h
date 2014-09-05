541d0b49674e3ce8152d1ddb3eebaa69950bdf37
Amit Maor <amaor16@cmc.edu>
2014-09-05 11:30:31 -0700

    Added some target value functionality

--- BullsEye/AMViewController.h
+++ BullsEye/AMViewController.h
@@ -14,4 +14,6 @@
 
 - (IBAction)sliderMoved:(UISlider *)slider;
 
+@property (nonatomic, weak) IBOutlet UISlider *slider;
+
 @end
--- BullsEye/AMViewController.m
+++ BullsEye/AMViewController.m
@@ -15,11 +15,14 @@
 @implementation AMViewController
 {
     int _currentValue;
+    int _targetValue;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    _currentValue = self.slider.value;
+    _targetValue = 1 + arc4random_uniform(100);
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -32,7 +35,8 @@
 - (IBAction)showAlert
 {
     NSString *message = [NSString stringWithFormat:
-                         @"The value of the slider is: %d", _currentValue];
+                         @"The value of the slider is: %d\nThe target value is: %d",
+                         _currentValue, _targetValue];
     UIAlertView *alertView = [[UIAlertView alloc]
       initWithTitle:@"Hello, World!"
       message:message
