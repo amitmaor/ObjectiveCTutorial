4abd5151a1f28ce6ca0f9af0699e8664ea595ce5
Amit Maor <amaor16@cmc.edu>
2014-09-05 10:12:52 -0700

    Initial Commit

---
+++ BullsEye/AMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AMAppDelegate.h
+//  BullsEye
+//
+//  Created by Amit Maor on 9/5/14.
+//  Copyright (c) 2014 Amit Maor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BullsEye/AMViewController.h
@@ -0,0 +1,13 @@
+//
+//  AMViewController.h
+//  BullsEye
+//
+//  Created by Amit Maor on 9/5/14.
+//  Copyright (c) 2014 Amit Maor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMViewController : UIViewController
+
+@end
