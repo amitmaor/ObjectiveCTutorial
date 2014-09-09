90ab16a43a3727e9beda96ea1e7578807b0571a1
Amit Maor <amaor16@cmc.edu>
2014-09-08 21:53:28 -0700

    Added About view and all necessary additional files

---
+++ BullsEye/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  BullsEye
+//
+//  Created by Amit Maor on 9/8/14.
+//  Copyright (c) 2014 Amit Maor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+- (IBAction)close;
+
+@end
